262b0e3671117d80de57635956f570d9cd2e9709
zigsong <wldms5764@gmail.com>
2020-09-26 00:15:12 +0900

    bj link test

--- jieun/bj1065/1065.cpp
+++ jieun/bj1065/1065.cpp
@@ -1,3 +1,5 @@
+<see cref="https://www.acmicpc.net/problem/1065" />
+
 #include <iostream>
 
 using namespace std;
@@ -5,14 +7,15 @@ using namespace std;
 int total;
 int cnt = 0;
 
-bool hansoo(int num) { // if 123
+bool hansoo(int num) { 
+  // 한자릿수, 두자릿수는 모두 이미 한수
   if (num < 100) return true;
 
   int n1, n2, n3;
 
-  n1 = num % 10; // 3
-  n2 = (num / 10) % 10; // 2
-  n3 = num / 100; // 1
+  n1 = num % 10; // 1의 자릿수
+  n2 = (num / 10) % 10; // 10의 자릿수
+  n3 = num / 100; // 100의 자릿수
 
   if (n3 - n2 == n2 - n1) return true;
   else return false;
