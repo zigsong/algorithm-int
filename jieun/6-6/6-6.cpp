bf8b55531cff3de59c48f970707ad6b960273e0f
zigsong <wldms5764@gmail.com>
2020-09-29 14:27:55 +0900

    study feedback

--- jieun/6-6/6-6.cpp
+++ jieun/6-6/6-6.cpp
@@ -12,12 +12,12 @@ const int coverType[4][3][2] = {
 };
 
 bool set(vector<vector<int>>& board, int y, int x, int type, int delta) {
-  bool ok = true;
+  bool ok = true;`
   for (int i=0; i<3; ++i) {
     const int ny = y + coverType[type][i][0];
     const int nx = x + coverType[type][i][1];
     if (ny<0 || ny>=board.size() || nx < 0 || nx>=board[0].size()) ok = false;
-    else if ((board[ny][nx] += delta) > 1) ok = false; // 이미 덮인 칸(board[y][x]=1)인데 또 덮으려(delta=1)할 때
+    else if ((board[ny][nx] += delta) > 1) ok = false; // 이미 덮인 칸(board[y][x]=1)인데 또 덮으려(delta=1)할 때 or 검은 칸에 덮으려고 할 때
   }
 
   return ok;
--- jieun/7-8/7-8.cpp
+++ jieun/7-8/7-8.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-vector<int> height;
+vector<int> height = {3, 8, 8, 8, 8, 6, 7, 5};
 
 int solve(int left, int right) {
   if (left == right) return height[left]; // base case: 판자가 1개밖에 없는 경우
@@ -12,7 +12,7 @@ int solve(int left, int right) {
   
   int lo = mid, hi = mid+1;
   int h = min(height[lo], height[hi]); // Q. 왜 작은 쪽으로 가는?
-  ret = max(ret, h*2);
+  ret = max(ret, h*2); // h*2 = (mid~mid+1)을 밑변으로 하는 사각형의 넓이
 
   while (left<lo || hi<right) {
     if (hi<right && (lo == left || height[lo-1] < height[hi+1])) {
@@ -25,4 +25,8 @@ int solve(int left, int right) {
     ret = max(ret, h*(hi-lo+1));
   }
   return ret;
+}
+
+int main() {
+  cout << solve(0, height.size()) << endl;  
 }
