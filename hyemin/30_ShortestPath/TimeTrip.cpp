51cc9192e8431811b60ab45d550c43a663523412
hyemmie <lhm0710@naver.com>
2020-11-28 15:27:11 +0900

    add 30

---
+++ hyemin/30_ShortestPath/TimeTrip.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<fstream>
+#include<cassert>
+#include<numeric>
+#include<set>
+#include<map>
+#include<queue>
+#include<list>
+#include<deque>
+using namespace std;
+
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define FORE(it,x) for(typeof(x.begin()) it=x.begin();it!=x.end();++it)
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define CLEAR(x,with) memset(x,with,sizeof(x))
+#define sz size()
+typedef long long ll;
+
+const int MAX_V = 100;
+const int INF = 987654321;
+
+// 정점의 개수
+int V; 
+// 그래프의 인접 리스트. (연결된 정점 번호, 간선 가중치) 쌍을 담는다.
+vector<pair<int,int> > adj[MAX_V];
