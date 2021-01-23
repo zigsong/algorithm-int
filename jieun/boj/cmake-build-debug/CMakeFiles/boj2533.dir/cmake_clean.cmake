4a1cb1b1f4ea00fcc64fee930a93db4f9e1b4e85
zigsong <wldms5764@gmail.com>
2021-01-23 17:02:49 +0900

    feat: boj21-22 finished

---
+++ jieun/boj/cmake-build-debug/CMakeFiles/boj2533.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boj2533.dir/boj2533.cpp.o"
+  "boj2533"
+  "boj2533.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boj2533.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
